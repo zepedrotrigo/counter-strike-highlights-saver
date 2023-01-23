f2d482a636536836091dff1815e0028e2a67718f
José Trigo <55859683+zepedrotrigo@users.noreply.github.com>
2023-01-23 00:04:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-var spotify_redirect_uri = 'https://counter-strike-highlights-saver-odntir186-zepedrotrigo.vercel.app/auth/callback'
+var spotify_redirect_uri = 'http://localhost:3488/auth/callback/auth/callback'
 
 var generateRandomString = function (length) {
 	var text = '';
